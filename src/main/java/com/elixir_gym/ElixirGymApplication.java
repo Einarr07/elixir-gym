fd5d72d61438f0a761222777e408ae1c248bf4f5
Einarr07 <aidenperse@gmail.com>
2025-08-15 10:39:42 -0500

    Initialization

---
+++ src/main/java/com/elixir_gym/ElixirGymApplication.java
@@ -0,0 +1,13 @@
+package com.elixir_gym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElixirGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElixirGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elixir_gym/ElixirGymApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elixir_gym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElixirGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
